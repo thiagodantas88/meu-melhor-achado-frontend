76dd4d674dbebd542f81316a8d2bfd98a525c228
Thiago Dantas <thiagodantas@MacBook-Air-de-Thiago.local>
2026-05-21 17:10:57 -0300

    fix: inicializa dataLayer do GA4 no cliente

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,27 +1,38 @@
 'use client'
 
+import { useEffect } from 'react'
 import Script from 'next/script'
+
+declare global {
+  interface Window {
+    dataLayer?: Array<IArguments | unknown[]>
+    gtag?: (...args: unknown[]) => void
+  }
+}
 
 export default function GoogleAnalytics() {
   const GA_ID = process.env.NEXT_PUBLIC_GA_ID
+
+  useEffect(() => {
+    if (!GA_ID) return
+
+    window.dataLayer = window.dataLayer || []
+    window.gtag = function gtag() {
+      window.dataLayer?.push(arguments)
+    }
+
+    window.gtag('js', new Date())
+    window.gtag('config', GA_ID, {
+      page_path: window.location.pathname,
+    })
+  }, [GA_ID])
+
   if (!GA_ID) return null
 
   return (
-    <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_ID}', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
-    </>
+    <Script
+      src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      strategy="afterInteractive"
+    />
   )
 }
